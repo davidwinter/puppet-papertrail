4255402fc3c0a4757816a95ca27916e26e911d2c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-05-12 00:57:02 -0700

    Using ensure_packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,21 +32,7 @@ class papertrail (
     notify => Service['rsyslog'],
   }
 
-  package { 'gcc':
-    ensure => present,
-  }
-
-  package { 'rubygems':
-    ensure => present,
-  }
-
-  package { 'libssl-dev':
-    ensure => present,
-  }
-
-  package { 'ruby-dev':
-    ensure => present,
-  }
+  ensure_packages(['gcc', 'rubygems', 'libssl-dev', 'ruby-dev'])
 
   package { 'remote_syslog':
     ensure   => present,
